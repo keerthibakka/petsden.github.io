dcf91093638c2c85dd0487229d9e7526fa7242a5
keerthibakka <keerthiramakrishnareddy@gmail.com>
2022-10-10 19:22:28 +0530

    my ngo website

---
+++ myscripts.js
@@ -0,0 +1,35 @@
+
+
+
+function giveinfo()
+{
+   
+    window.open("info.html");
+}
+
+function thank()
+{
+    window.open("thanku.html");
+    alert("Our Team will reach you out soon!")
+}
+function sorry()
+{
+    window.open("sorry.html");
+    alert("Our Team will reach you out soon!")
+}
+function wewill()
+{
+    window.open("thank.html");
+    alert("Our Team will reach soon visit you and relocate the animal!")
+}
+
+function work()
+{
+    window.open("work.html");
+    alert("This may take a few minutes!")
+}
+function donate()
+{
+    window.open("thank.html");
+    alert("Thanks for donating!")
+}
